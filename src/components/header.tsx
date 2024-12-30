86bd2b0822554e5bd3d73d22b22693ca474f91e5
Andrew Vlietstra <vlietstra123@hotmail.com>
2024-12-30 17:24:09 -0500

    Added prettier tailwind class sorting extension, started styling header with basic classes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,24 @@
 import Link from "next/link";
 import Logo from "./logo";
+import { routes } from "@/lib/contants";
 
 export default function Header() {
-	return (
-		<header>
-			<Logo />
-			<Link href="/">Home</Link>
-			<Link href="/events/all">All Events</Link>
-		</header>
-	);
+  return (
+    <header className="flex h-14 items-center justify-between border-b border-white/10 px-4 md:px-8">
+      <Logo />
+
+      <nav>
+        <ul className="flex gap-x-6 text-base">
+          {routes.map((route) => (
+            <li
+              key={route.name}
+              className="text-white/50 transition hover:text-white"
+            >
+              <Link href={route.path}>{route.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
 }
---
+++ src/lib/contants.ts
@@ -0,0 +1,10 @@
+export const routes = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "All Events",
+    path: "/events/all",
+  },
+];
