2b7f861afec81c3171d337a05503bc8b9d0fa9d2
Andrew Vlietstra <vlietstra123@hotmail.com>
2024-12-31 14:53:34 -0500

    In events page get access to url params then conditionally render H1 based on if url param city is specific city or all, adjusted scroll styling in global css and main layout files

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,9 +1,21 @@
 import H1 from "@/components/h1";
+import { capitalizeString } from "@/lib/utility";
 
-export default function EventsPage() {
+type EventsPageProps = {
+  params: {
+    city: string;
+  };
+};
+
+export default function EventsPage({ params }: EventsPageProps) {
+  const { city } = params;
+
   return (
-    <main>
-      <H1>Events in Austin</H1>
+    <main className="flex min-h-[110vh] flex-col items-center px-[20px] py-24">
+      <H1>
+        {city === "all" && "All Events"}
+        {city !== "all" && `Events in ${capitalizeString(city)}`}
+      </H1>
     </main>
   );
 }
