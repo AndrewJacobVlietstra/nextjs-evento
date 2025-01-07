7d286476fa9d4c188fdf9df595902017334d7895
Andrew Vlietstra <vlietstra123@hotmail.com>
2025-01-07 15:05:12 -0500

    Created skeleton card component to mimic event cards to show user while page is fetching data/loading, also used an array trick to cleanly render 6 of these skeletonCard components while loading

--- src/app/events/[city]/loading.tsx
+++ src/app/events/[city]/loading.tsx
@@ -1,5 +1,11 @@
-import H1 from "@/components/h1";
+import SkeletonCard from "@/components/skeleton-card";
 
 export default function Loading() {
-  return <H1>Loading events...</H1>;
+  return (
+    <div className="mx-auto flex max-w-[1100px] flex-wrap justify-center gap-20 px-[20px] py-24">
+      {Array.from({ length: 6 }).map((item, i) => (
+        <SkeletonCard key={i} />
+      ))}
+    </div>
+  );
 }
