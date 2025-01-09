8991106be364c78da69a994228fb3dd6c6361fea
Andrew Vlietstra <vlietstra123@hotmail.com>
2025-01-08 20:19:17 -0500

    Moved data fetching from events page into events-list component, wrapped EventsList in a suspense with fallback to loading.tsx to not block other elements such as the H1 when first landing on the page

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,8 +1,8 @@
 import H1 from "@/components/h1";
 import EventsList from "@/components/events-list";
-import { BASE_API_URL } from "@/lib/contants";
-import { EventApiResponse } from "@/lib/types";
 import { capitalizeString } from "@/lib/utility";
+import { Suspense } from "react";
+import Loading from "./loading";
 
 type EventsPageProps = {
   params: {
@@ -13,9 +13,6 @@ type EventsPageProps = {
 export default async function EventsPage({ params }: EventsPageProps) {
   const { city } = params;
 
-  const result = await fetch(`${BASE_API_URL}?city=${city}`);
-  const events: EventApiResponse[] = await result.json();
-
   return (
     <main className="flex min-h-[110vh] flex-col items-center px-[20px] py-24">
       <H1 className="mb-28">
@@ -23,7 +20,9 @@ export default async function EventsPage({ params }: EventsPageProps) {
         {city !== "all" && `Events in ${capitalizeString(city)}`}
       </H1>
 
-      <EventsList events={events} />
+      <Suspense fallback={<Loading />}>
+        <EventsList city={city} />
+      </Suspense>
     </main>
   );
 }
--- src/components/events-list.tsx
+++ src/components/events-list.tsx
@@ -1,11 +1,15 @@
 import { EventApiResponse } from "@/lib/types";
 import EventCard from "./event-card";
+import { BASE_API_URL } from "@/lib/contants";
 
 type EventsListProps = {
-  events: EventApiResponse[];
+  city: string;
 };
 
-export default function EventsList({ events }: EventsListProps) {
+export default async function EventsList({ city }: EventsListProps) {
+  const result = await fetch(`${BASE_API_URL}?city=${city}`);
+  const events: EventApiResponse[] = await result.json();
+
   return (
     <section className="flex max-w-[1100px] flex-wrap justify-center gap-10 px-[20px]">
       {events.map((event) => (
