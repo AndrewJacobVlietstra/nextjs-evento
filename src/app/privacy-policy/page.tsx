85ad01f73d65aa78de5058b262de90757d32df2e
Andrew Vlietstra <vlietstra123@hotmail.com>
2025-01-16 15:24:13 -0500

    Added boilerplate template content to privacy-policy page and terms-conditions page so there is some content there to make it seem more realistic.

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -1,3 +1,64 @@
+import H1 from "@/components/h1";
+
 export default function PrivacyPolicyPage() {
-	return <main>Privacy Policy Page</main>;
+  return (
+    <main className="flex max-w-[60rem] flex-col gap-y-8 px-16 py-16 leading-relaxed">
+      <H1>Privacy Notice</H1>
+
+      <p>
+        Last updated:{" "}
+        <span className="font-bold text-blue-200">
+          {new Date().toLocaleDateString("en-US", {
+            year: "numeric",
+            month: "short",
+            day: "2-digit",
+          })}
+        </span>
+      </p>
+
+      <p className="">
+        This privacy notice for EventoEvents (doing business as Evento) (“
+        <b>Company</b>,” “<b>we</b>,” “<b>us</b>,” or “<b>our</b>“), describes
+        how and why we might collect, store, use, and/or share (“<b>process</b>
+        “) your information when you use our services (“<b>Services</b>“), such
+        as when you:
+      </p>
+
+      <ul className="list-outside list-disc pl-5">
+        <li>
+          Visit our website at{" "}
+          <a className="text-blue-400" href="#">
+            evento.com
+          </a>
+          , or any website of ours that links to this privacy notice
+        </li>
+
+        <li>
+          Download and use our application(s), such as our mobile application —
+          <b>EventoMobile</b>, or any other application of ours that links to
+          this privacy notice
+        </li>
+
+        <li>
+          Engage with us in other related ways ― including any sales, marketing,
+          or events
+        </li>
+      </ul>
+
+      <p>
+        Questions or concerns? Reading this privacy notice will help you
+        understand your privacy rights and choices. If you do not agree with our
+        policies and practices, please do not use our Services. If you still
+        have any questions or concerns, please contact us at{" "}
+        <a className="text-blue-400" href="#">
+          evento-customersupport@gmail.com
+        </a>
+        .
+      </p>
+
+      <p>
+        This privacy policy was created by <b>EventoEvents</b>.
+      </p>
+    </main>
+  );
 }
--- src/app/terms-conditions/page.tsx
+++ src/app/terms-conditions/page.tsx
@@ -1,3 +1,81 @@
-export default function TermsConditionsPage() {
-	return <main>Terms & Conditions Page</main>;
+import H1 from "@/components/h1";
+
+export default function PrivacyPolicyPage() {
+  return (
+    <main className="flex max-w-[60rem] flex-col gap-y-7 px-16 py-16 leading-relaxed">
+      <H1>Evento Terms and Conditions</H1>
+
+      <p>
+        Last updated:{" "}
+        <span className="font-bold text-blue-200">
+          {new Date().toLocaleDateString("en-US", {
+            year: "numeric",
+            month: "short",
+            day: "2-digit",
+          })}
+        </span>
+      </p>
+
+      <H1 className="text-2xl lg:text-2xl">AGREEMENT TO TERMS</H1>
+
+      <p>
+        These Terms and Conditions constitute a legally binding agreement made
+        between you, whether personally or on behalf of an entity (“you”) and
+        EventoEvents (“we,” “us” or “our”), concerning your access to and use of
+        the{" "}
+        <a className="text-blue-400" href="#">
+          evento.com
+        </a>{" "}
+        website as well as any other media form, media channel, mobile website
+        or mobile application related, linked, or otherwise connected thereto
+        (collectively, the “Site”).
+      </p>
+
+      <p>
+        You agree that by accessing the Site, you have read, understood, and
+        agree to be bound by all of these Terms and Conditions. If you do not
+        agree with all of these Terms and Conditions, then you are expressly
+        prohibited from using the Site and you must discontinue use immediately.
+      </p>
+
+      <H1 className="text-2xl lg:text-2xl">INTELLECTUAL PROPERTY RIGHTS</H1>
+
+      <p>
+        Unless otherwise indicated, the Site is our proprietary property and all
+        source code, databases, functionality, software, website designs, audio,
+        video, text, photographs, and graphics on the Site (collectively, the
+        “Content”) and the trademarks, service marks, and logos contained
+        therein (the “Marks”) are owned or controlled by us or licensed to us,
+        and are protected by copyright and trademark laws and various other
+        intellectual property rights and unfair competition laws of the United
+        States, foreign jurisdictions, and international conventions.
+      </p>
+
+      <H1 className="text-2xl lg:text-2xl">PROHIBITED ACTIVITIES</H1>
+
+      <p>
+        You may not access or use the Site for any purpose other than that for
+        which we make the Site available. The Site may not be used in connection
+        with any commercial endeavors except those that are specifically
+        endorsed or approved by us.
+      </p>
+
+      <p>As a user of the Site, you agree not to:</p>
+
+      <ol className="list-decimal pl-5">
+        <li>
+          Systematically retrieve data or other content from the Site to create
+          or compile, directly or indirectly, a collection, compilation,
+          database, or directory without written permission from us.
+        </li>
+        <li>
+          Use a buying agent or purchasing agent to make purchases on the Site.
+        </li>
+        <li>
+          Trick, defraud, or mislead us and other users, especially in any
+          attempt to learn sensitive account information such as user passwords;
+        </li>
+      </ol>
+    </main>
+  );
 }
